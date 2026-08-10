570c8f63ff9e6ddeb6977e9dc7b294e14b5bae81
Rahul Kumar <rptciitr@gmail.com>
2026-08-10 17:37:48 +0530

    Time: 0 ms (100%), Space: 77 MB (94.14%) - LeetHub

--- 0002-add-two-numbers/0002-add-two-numbers.cpp
+++ 0002-add-two-numbers/0002-add-two-numbers.cpp
@@ -10,71 +10,27 @@
  */
 class Solution {
 
-    ListNode* insertAtHead(ListNode* &head,int digit){
-        ListNode* temp = new ListNode(digit);
-        if(head == NULL){
-            head = temp;
-        }else{
-            temp->next = head;;
-            head = temp;
-        }
-        return head;
-    }
-
-    ListNode* add(ListNode* l1, ListNode* l2){
-        int carry = 0;
-        int sum = 0;
-        ListNode* head = NULL;
-        while(l1 != NULL && l2 != NULL){
-            sum = l1->val + l2->val + carry;
-            int digit = sum%10;
-            carry = sum/10;
-            insertAtHead(head,digit);
-            l1 = l1->next;
-            l2 = l2->next;
-        }
-        while(l1 != NULL){
-            sum = carry + l1->val;
-            int digit = sum%10;
-            carry = sum/10;
-            insertAtHead(head,digit);
-            l1 = l1->next;
-        }
-        while(l2 != NULL){
-            sum = carry + l2->val;
-            int digit = sum%10;
-            carry = sum/10;
-            insertAtHead(head,digit);
-            l2 = l2->next;
-        }
-        while(carry != 0){
-            sum = carry;
-            int digit = sum%10;
-            insertAtHead(head,digit);
-            carry = sum/10;
-        }
-        return head;
-    }
-
-    ListNode* reverse(ListNode* head){
-        ListNode* curr = head;
-        ListNode* prev = NULL;
-        ListNode* nxt = NULL;
-        while(curr != NULL){
-            nxt = curr->next;
-            curr->next = prev;
-            prev = curr;
-            curr = nxt;
-        }
-        return prev;
-    }
-
 public:
     ListNode* addTwoNumbers(ListNode* l1, ListNode* l2) {
-        if(l1 == NULL) return l2;
-        if(l2 == NULL) return l1;
-        ListNode *ans = add(l1,l2);
-        ans = reverse(ans);
-        return ans;
+        ListNode* head = NULL;
+        ListNode* tail = NULL;
+        int carry = 0,sum=0;
+        while(l1!=NULL || l2!=NULL || carry!=0){
+             int val1 = (l1!=NULL) ? l1->val : 0;
+             int val2 = (l2!=NULL) ? l2->val : 0;
+             sum = val1 + val2 + carry;
+             ListNode* newNode = new ListNode(sum%10);
+             if(head==NULL){
+                head = newNode;
+                tail = newNode;
+             }else{
+                tail->next = newNode;
+                tail = newNode;
+             }
+             carry = sum/10;
+             if(l1!=NULL) l1 = l1->next;
+             if(l2!=NULL) l2 = l2->next;
+        }
+        return head;
     }
 };
