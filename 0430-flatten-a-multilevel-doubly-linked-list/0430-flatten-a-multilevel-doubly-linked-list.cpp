23a6ccbe58af2d15575852868b1f80c05b3c40cd
Rahul Kumar <rptciitr@gmail.com>
2026-08-13 11:55:00 +0530

    Time: 0 ms (100%), Space: 11.6 MB (75.51%) - LeetHub

--- 0430-flatten-a-multilevel-doubly-linked-list/0430-flatten-a-multilevel-doubly-linked-list.cpp
+++ 0430-flatten-a-multilevel-doubly-linked-list/0430-flatten-a-multilevel-doubly-linked-list.cpp
@@ -12,22 +12,21 @@ public:
 class Solution {
 public:
     Node* flatten(Node* head) {
-        if(head == NULL){
+        if (head == NULL)
             return head;
-        }
         Node* curr = head;
-        while(curr != NULL){
-            if(curr->child != NULL){
-                Node* next = curr->next;
+        while (curr != NULL) {
+            Node* next = curr->next;
+            if (curr->child != NULL) {
                 curr->next = flatten(curr->child);
                 curr->next->prev = curr;
                 curr->child = NULL;
-                while(curr->next != NULL){
+                while (curr->next != NULL) {
                     curr = curr->next;
                 }
-                if(next != NULL){
-                    curr->next = next;
-                    next->prev = curr;
+                curr->next = next;
+                if (curr->next != NULL) {
+                    curr->next->prev = curr;
                 }
             }
             curr = curr->next;
