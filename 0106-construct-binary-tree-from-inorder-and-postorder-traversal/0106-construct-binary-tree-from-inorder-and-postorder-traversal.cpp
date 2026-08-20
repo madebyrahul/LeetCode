63be860b867ac88a21790dcbd29edd98a167949a
Rahul Kumar <rptciitr@gmail.com>
2026-08-21 01:05:41 +0530

    Time: 3 ms (69.48%), Space: 27.5 MB (33.82%) - LeetHub

--- 0106-construct-binary-tree-from-inorder-and-postorder-traversal/0106-construct-binary-tree-from-inorder-and-postorder-traversal.cpp
+++ 0106-construct-binary-tree-from-inorder-and-postorder-traversal/0106-construct-binary-tree-from-inorder-and-postorder-traversal.cpp
@@ -11,23 +11,20 @@
  */
 class Solution {
 
-    int find(vector<int>& inorder,int s,int e,int val){
-        for(int i=s;i<=e;i++){
-            if(inorder[i] == val){
-                return i;
-            }
+    void createMapping(vector<int>& inorder,map<int,int> &nodeToIndex,int size){
+        for(int i=0;i<size;i++){
+            nodeToIndex[inorder[i]] = i;
         }
-        return -1;
     }
 
-    TreeNode* solve(vector<int>& inorder, vector<int>& postorder,int postIdx,int inStart,int inEnd){
+    TreeNode* solve(vector<int>& inorder, vector<int>& postorder,int postIdx,int inStart,int inEnd,map<int,int> &nodeToIndex){
         if(inStart>inEnd){
             return NULL;
         }
         TreeNode* root = new TreeNode(postorder[postIdx]);
-        int position = find(inorder,inStart,inEnd,postorder[postIdx]);
-        root->right = solve(inorder,postorder,postIdx-1,position+1,inEnd);
-        root->left = solve(inorder,postorder,postIdx - (inEnd-position) -1,inStart,position-1);
+        int position = nodeToIndex[postorder[postIdx]];
+        root->right = solve(inorder,postorder,postIdx-1,position+1,inEnd,nodeToIndex);
+        root->left = solve(inorder,postorder,postIdx - (inEnd-position) -1,inStart,position-1,nodeToIndex);
         return root;
     }
     
@@ -36,7 +33,9 @@ public:
     TreeNode* buildTree(vector<int>& inorder, vector<int>& postorder) {
         int postIdx = postorder.size()-1;
         int inStart = 0,inEnd = inorder.size()-1;
-        TreeNode* ans  = solve(inorder,postorder,postIdx,inStart,inEnd);
+        map<int,int> nodeToIndex;
+        createMapping(inorder,nodeToIndex,inorder.size());
+        TreeNode* ans  = solve(inorder,postorder,postIdx,inStart,inEnd,nodeToIndex);
         return ans;
     }
 };
