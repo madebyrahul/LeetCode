7be25f4a89d1ed1a9783ffdfcedcf16513dfd64a
Rahul Kumar <rptciitr@gmail.com>
2026-08-30 23:58:50 +0530

    Time: 0 ms (100%), Space: 8.5 MB (93.31%) - LeetHub

--- 0050-powx-n/0050-powx-n.cpp
+++ 0050-powx-n/0050-powx-n.cpp
@@ -1,28 +1,15 @@
 class Solution {
 public:
     double myPow(double x, int n) {
-        long long exponent = n;
-        if (exponent < 0) {
-            exponent = -exponent;
-        }
-
-        double result = 1.0;
-        double base = x;
-
-        while (exponent > 0) {
-            if (exponent % 2 == 1) {
-                result = result * base;
-                exponent = exponent - 1;
-            } else {
-                base = base * base;
-                exponent = exponent / 2;
+        long y = abs((long)n);
+        double ans = 1.0;
+        while(y>=1){
+            if(y&1){
+                ans = ans*x;
             }
+            x = x*x;
+            y = y/2;
         }
-
-        if (n < 0) {
-            result = 1.0 / result;
-        }
-
-        return result;
+        return n<0 ? 1/ans : ans;
     }
 };
