e320dac4167eb7cda916385d80fa6e8e7c3c5c27
Rahul Kumar <rptciitr@gmail.com>
2026-09-03 23:38:35 +0530

    Time: 72 ms (90.57%), Space: 46.5 MB (40.08%) - LeetHub

--- 0743-network-delay-time/0743-network-delay-time.cpp
+++ 0743-network-delay-time/0743-network-delay-time.cpp
@@ -1,20 +1,23 @@
 class Solution {
 public:
     int networkDelayTime(vector<vector<int>>& times, int n, int k) {
-        unordered_map<int,list<pair<int,int>>> adjList;
+        unordered_map<int,list<pair<int,int>>> adjList; 
         for(int i=0;i<times.size();i++){
             int u = times[i][0];
             int v = times[i][1];
             int w = times[i][2];
             adjList[u].push_back({v,w});
         }
+
         vector<int> time(n+1);
         for(int i=1;i<=n;i++){
             time[i] = INT_MAX;
         }
+
         time[k] = 0;
-        set<pair<int,int>> st;
+        set<pair<int,int>> st; // here value is like {time,node} in this set.
         st.insert({0,k});
+
         while(!st.empty()){
             auto top = *(st.begin());
             int nodeTime = top.first;
@@ -31,10 +34,12 @@ public:
                 }
             }
         }
+
         int ans = INT_MIN;
         for(int i=1;i<=n;i++){
             ans = max(ans,time[i]);
         }
+
         return ans == INT_MAX ? -1 : ans;
     }
 };
