9a67f44e9b6d05cc79be692532e0381df369db07
Rahul Kumar <rptciitr@gmail.com>
2026-09-17 03:14:42 +0530

    Time: 0 ms (100%), Space: 8.2 MB (11.29%) - LeetHub

--- 0509-fibonacci-number/0509-fibonacci-number.cpp
+++ 0509-fibonacci-number/0509-fibonacci-number.cpp
@@ -2,17 +2,21 @@ class Solution {
 
     int solve(int n,vector<int> &dp){
         if(n<=1) return n;
+
         if(dp[n] != -1) return dp[n];
         dp[n] = solve(n-1,dp) + solve(n-2,dp);
+
         return dp[n];
     }
 
 public:
     int fib(int n) {
+        // solved by top-down approach of DP
         vector<int> dp(n+1);
         for(int i=0;i<n+1;i++){
             dp[i] = -1;
         }
+
         return solve(n,dp);
     }
 };
