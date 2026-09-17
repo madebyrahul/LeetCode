ea19828023d593dd408be3afd16ec023e894318a
Rahul Kumar <rptciitr@gmail.com>
2026-09-18 00:18:03 +0530

    Time: 0 ms (100%), Space: 7.8 MB (73.7%) - LeetHub

---
+++ 0070-climbing-stairs/0070-climbing-stairs.cpp
@@ -0,0 +1,19 @@
+class Solution {
+
+    int solve(int n){
+        if(n == 0) return 1; 
+        int prev2 = 1;
+        int prev1 = 1;
+        for(int i=2;i<=n;i++){
+            int curr = prev1 + prev2;
+            prev2 = prev1;
+            prev1 = curr;
+        }
+        return prev1;
+    }
+
+public:
+    int climbStairs(int n) {
+        return solve(n);
+    }
+};
